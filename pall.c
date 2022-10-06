549a6ff85498c91cfc9a4223561b653ff3864f16
Michiepaiper <106969102+Michiepaiper@users.noreply.github.com>
2022-10-06 15:25:21 +0100

    pall.c

--- pall.c
+++ pall.c
@@ -1,13 +1,20 @@
 #include "monty.h"
-
 /**
- * pall - prints the stack
- * @stack: stack given by main in start.c
- * @line_cnt: amount of lines
- *
- * Return: void
+ * pall - function that prints all the values on the stack
+ * @stack: top of the stack
+ * @line_number: # of the line in the .m file
  */
-void pall(stack_t **stack, unsigned int line_cnt __attribute__((unused)))
+void pall(stack_t **stack, unsigned int line_number)
 {
-	print_stack(*stack);
+	stack_t *current;
+
+	current = *stack;
+
+	(void)line_number;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+	}
 }
